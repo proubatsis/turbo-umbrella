d09baee153709afd5929a1bcca453047ebaa58a0
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-09-09 13:08:27 -0400

    Put rate and hours in invoice item

--- init.sql
+++ init.sql
@@ -7,14 +7,9 @@ CREATE TABLE invoice_item (
     id integer primary key autoincrement,
     invoice_id integer not null references invoice(id),
     title text not null,
-    work_date date not null
-);
-
-CREATE TABLE timesheet_entry (
-    id integer primary key autoincrement,
-    invoice_item_id not null references invoice_item(id),
-    entry text,
-    hours decimal
+    work_date date not null,
+    rate decimal not null,
+    hours decimal not null
 );
 
 INSERT INTO SQLITE_SEQUENCE VALUES ('invoice', 100);
